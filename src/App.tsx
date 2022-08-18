7dbc6e2df331bc05aaaa7fac894a31a1eb7dbb5c
woobin <woobin@dynamicmedia.co.kr>
2022-08-18 19:42:22 +0900

    20220818

--- src/App.tsx
+++ src/App.tsx
@@ -6,24 +6,24 @@ const Wrapper = styled.div`
   height: 100vh;
   width: 100vw;
   display: flex;
+  flex-direction: column;
   justify-content: space-around;
   align-items: center;
 `;
 
 const Grid = styled.div`
   display: grid;
-  grid-template-columns: repeat(3, 1fr);
+  grid-template-columns: repeat(2, 1fr);
   width: 50vw;
   gap: 10px;
-  div:first-child,
-  div:last-child {
-    grid-column: span 2;
-  }
 `;
 
 const Box = styled(motion.div)`
-  background-color: rgba(255, 255, 255, 1);
-  border-radius: 40px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: rgba(255, 255, 255, 0.75);
+  border-radius: 8px;
   height: 200px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
@@ -37,6 +37,35 @@ const Overlay = styled(motion.div)`
   align-items: center;
 `;
 
+const Switch = styled.button<{ clicked: boolean }>`
+  background-color: white;
+  border: 0;
+  border-radius: 4px;
+  padding: 6px 8px;
+  cursor: pointer;
+  transition: all 0.2s;
+  transform: ${(props) => (props.clicked ? "scale(1.15)" : "")};
+  font-size: 14px;
+  font-weight: bold;
+  color: ${(props) => (props.clicked ? "#dd3a3a" : "#2a63ff")};
+`;
+
+const Circle = styled(motion.div)`
+  background-color: white;
+  height: 48px;
+  width: 48px;
+  border-radius: 24px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+const boxVariants = {
+  hover: (custom: string) => ({
+    originX: custom === "1" || custom === "3" ? 1 : 0,
+    originY: custom === "1" || custom === "2" ? 1 : 0,
+    scale: 1.1,
+  }),
+};
+
 const overlay = {
   hidden: { backgroundColor: "rgba(0, 0, 0, 0)" },
   visible: { backgroundColor: "rgba(0, 0, 0, 0.5)" },
@@ -44,12 +73,24 @@ const overlay = {
 };
 
 function App() {
+  const [clicked, setClicked] = useState(false);
+  const toggleClicked = () => setClicked((prev) => !prev);
   const [id, setId] = useState<null | string>(null);
   return (
     <Wrapper>
       <Grid>
         {["1", "2", "3", "4"].map((n) => (
-          <Box onClick={() => setId(n)} key={n} layoutId={n} />
+          <Box
+            variants={boxVariants}
+            whileHover="hover"
+            onClick={() => setId(n)}
+            key={n}
+            layoutId={n}
+            custom={n}
+          >
+            {n === "2" && !clicked ? <Circle layoutId="circle" /> : null}
+            {n === "3" && clicked ? <Circle layoutId="circle" /> : null}
+          </Box>
         ))}
       </Grid>
       <AnimatePresence>
@@ -61,10 +102,20 @@ function App() {
             animate="visible"
             exit="exit"
           >
-            <Box layoutId={id} style={{ width: 400, height: 200 }} />
+            <Box
+              layoutId={id}
+              style={{
+                width: 400,
+                height: 200,
+                backgroundColor: "rgba(255, 255, 255, 1)",
+              }}
+            />
           </Overlay>
         ) : null}
       </AnimatePresence>
+      <Switch clicked={clicked} onClick={toggleClicked}>
+        Switch
+      </Switch>
     </Wrapper>
   );
 }
